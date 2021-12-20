40ee4c55765f15e8316d4bfac6f331545eddc0b0
Junoon21 <ritikasingh9006@gmail.com>
2021-12-20 11:38:56 +0530

    committed at 11:38 on 20 Dec

--- block-BJaabb/code/index.js
+++ block-BJaabb/code/index.js
@@ -5,23 +5,67 @@
 3. Declare variable `age` and `isLearning` with value `22` and `true` in the same line. hint: declaring multiplemvariable in one line.
 4. Now change the value of `user` to `John` and log the value of both `user` and `userName`.
 */
-
+var user=prompt("enter name:");
+alert(`the name is ${user}`);
+var userName=user;
+console.log(`the values are ${user} ${userName}`);
+var age=22,isLearning=true;
+user=`john`;
+console.log(`the values of user and username ${user} ${userName}`);
 /* 2.
 Use `prompt` to take two numbers from user and store it in variable `numA` and `numB`. Create a third variable called `sum`, using `+` operator add numA and numB , store it in varible `sum` and using alert display `sum` in browser.
 */
+var numA=Number(prompt("enter numa"));
+var numB=Number(prompt("enter numB"));
+var sum=numA+numB;
+alert(`the sum is ${sum}`);
+
+
 
 // Loops
 
 // 3. Complete the following code to make the output be 0 2 4 6 8 10?
 
-for (let j = 0; j <= 10; ____) console.log(j);
+for (let j = 0; j <= 10; j++) 
+{    if(j%2===0)
+    {
+        console.log(j);
+    }
+   
+}
+
 
 // 4. Print the odd numbers from 9 to 1 (both inclusive) using a for loop.
+for (let j = 10; j > 0; j--) 
+{if(j%2!==0)
+    {
+        console.log(j);
+    }
+   
+}
 
 // 5. Concat all the numbers from 5 to 0 (both inclusive) in descending order using a while loop. (543210)
+let sum=0;
+let i=5;
+while(i>=0)
+{
+    var sum=sum+"i";
+    alert(`sum= ${sum}`);
+    i--;
+}
 
 // 6. Concat all the even numbers from 10 to 0 (both inclusive) in descending order using a while loop. (1086420)
-
+let sum=0;
+let i=10;
+while(i>=0)
+{if(i%2===0)
+    {
+        var sum=sum+"i";
+        alert(`sum= ${sum}`);
+    }
+    i--;
+    
+}
 // Comparisoin
 
 /* 7. Take two value using prompt and store them in variables `num1` and `num2`. Check whether they are equal or not.
@@ -36,3 +80,16 @@ Example:
 "hello", -21; // false
 
 */
+var num1=prompt(`enter num1`);
+var num2=prompt(`enter num2`);
+if(num1===num2)
+{
+    alert(`equal`);
+}
+else {
+    alert(`not equal`);
+}
+if ((num1===`true`||num1===`null`|num1===`undefined`)|| (num1===`true`||num1===`null`|num1===`undefined`))
+{
+    alert(`Enter a valid value`);
+}
