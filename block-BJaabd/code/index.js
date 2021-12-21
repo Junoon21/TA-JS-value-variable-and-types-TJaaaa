188250d449957394f434d5eb3b54c382e0442530
Junoon21 <ritikasingh9006@gmail.com>
2021-12-21 23:09:38 +0530

    commit

--- block-BJaabd/code/index.js
+++ block-BJaabd/code/index.js
@@ -10,6 +10,27 @@ Write a program that asks the user his/her age and check for the following condi
 - `if` the age is greater than 55 then print the message " You are too old to participate in the marthon".
 👇
 */
+let age=+prompt("enter your age?");
+switch (true)
+{
+  case ( age >= 12 && age <= 55 ):{
+    alert("You can participate in the marathon");
+    break;
+  }
+  case (age>=4 && age<=11):{
+    alert(" You are too young to participate in the marathon");
+    break;
+  }
+  case (age<4):{
+    alert(" Hey Kiddo! Can You Walk ?");
+    break;
+  }
+  case (age>55):{
+    alert(" You are too old to participate in the marthon");
+    break;
+  }
+ 
+}
 
 // Loops
 /*
@@ -21,12 +42,33 @@ n = 7 => output: heeeeeeello
 👇
 */
 // [Your code goes here]
+let num=+prompt("give a positive integer.");
+let word="h";
+let midWord="e"
+for(let i=0;i<=num;i++)
+{   word=word+midWord;
+  if(i===num){
+    alert(midWord);
+  }
+  else{
+    
+  }
+
+  }
+
+
 
 /*
 Program to calculate the sum of first n natural numbers(1,2,3...n are known as natural numbers). Prompt user to enter n (using `prompt`) then based on input provided calculate and show result in `alert`.
 👇
 */
 // [Your code goes here]
+let num=+prompt("enter the value of n");
+let sum=0;
+for(let i=0;i<=num;i++)
+{
+  sum=sum+i;
+}
 
 /* Switch Statement
 
@@ -46,6 +88,30 @@ Take a number value from user and alert the message if it matches the conditions
 
 */
 // [Your code goes here]
+let num=+prompt("enter value");
+switch(true)
+{
+  case 1 :alert("one");
+  break;
+  case 2 :alert("two");
+  break;
+  case 3 :alert("three");
+  break;
+  case 4 :alert("four");
+  break;
+  case 5 :alert("five");
+  break;
+  case 6 :alert("six");
+  break;
+  case 7 :alert("seven");
+  break;
+  case 8 :alert("eight");
+  break;
+  case 9 :alert("nine");
+  break;
+  case 10 :alert("ten");
+  break;
+}
 
 /*
 🎖Using switch statement do the following
@@ -62,6 +128,17 @@ Take the value of `marks` (0-100) from user using `prompt` and `alert` the messa
 */
 
 // [Your code goes here]
+let marks=+prompt("enter marks");
+switch(true){
+  case marks>90 : alert("AA");
+  break;
+  case marks>80 && marks<=90 : alert("AA");
+  break;
+  case marks>70 && marks<=80 : alert("AA");
+  break;
+  case marks>90 : alert("AA");
+  break;
+}
 
 /*
  🎖Write a JavaScript program that takes two `integers` from user (using prompt) and alerts the larger number.
